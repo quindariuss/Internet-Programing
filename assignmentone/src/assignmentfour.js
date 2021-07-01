c1f175dd00ea65cb6b436767549c08ea42db716c
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-01 17:17:23 +0000

    made a needed link back to the home paga and spruced things up a bit

--- assignmentone/src/assignmentfour.js
+++ assignmentone/src/assignmentfour.js
@@ -1,9 +1,10 @@
-import { Heading, Text, Center, VStack } from "@chakra-ui/react";
+import { Heading, Text, Center, VStack, Box, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import ReactEmbedGist from "react-embed-gist";
 
 export function AssignmentFour() {
   return (
-    <>
+    <Box bgGradient="linear(to-l, #7928CA, #FF0080)">
       <Center
         pt="15"
         marginBottom="5"
@@ -11,22 +12,32 @@ export function AssignmentFour() {
         marginLeft={["5%", "15%", "25%"]}
       >
         <VStack spacing="10">
-          <Heading>Welcome to Assignment Four</Heading>
-          <Text>
+          <Heading size="md" color="white">
+            Welcome to Assignment Four
+          </Heading>
+          <Text color="white">
             Here you be over to puruse the code that allows you to connect to
             the database provided for the assignment that is written in python
             script.
           </Text>
-          <ReactEmbedGist
-            gist="quinwoods/fdb3eefdd6488a12830a8b40c6606525"
-            wrapperClass="gist__bash"
-            loadingClass="loading__screen"
-            titleClass="gist__title"
-            contentClass="gist__content"
-            errorClass="gist__error"
-          />
+          <Link to="/home">
+            <Button m="10">Back to home</Button>
+          </Link>
+          <Box boxSize="sm">
+            <ReactEmbedGist
+              gist="quinwoods/fdb3eefdd6488a12830a8b40c6606525"
+              wrapperClass="gist__bash"
+              loadingClass="loading__screen"
+              titleClass="gist__title"
+              contentClass="gist__content"
+              errorClass="gist__error"
+            />
+          </Box>
         </VStack>
       </Center>
-    </>
+      <Box boxSize="4xl" />
+      <Box boxSize="4xl" />
+      <Box boxSize="4xl" />
+    </Box>
   );
 }
