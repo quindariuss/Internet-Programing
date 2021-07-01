7d1153b8778b0bff972a3b807a1f3c1945a7d366
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-01 10:51:31 -0400

    finished displaying information from sql

--- assignmentfour/Database/main.py
+++ assignmentfour/Database/main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import sqlite3
 
+# conn=sqlite3.connect('sqlite-sakila.sq') c=conn.cursor() sql="select * from actor" data=c.execute(sql)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+conn= sqlite3.connect('sqlite-sakila.sq')
 
+c=conn.cursor()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sql="select * from actor"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+data = c.execute(sql)
+
+for row in data:
+    print(row)
+
+    conn = sqlite3.connect('sqlite-sakila.sq')
+    c = conn.cursor()
+    sql = "select * from actor"
+    data = c.execute(sql)
+
+    for row in data:
+        print(row)
+
+    print("==" * 50)
+
+    for row in data: print(row)
