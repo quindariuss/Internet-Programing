8ec3c64fd2d2bf3b1571764a0d278cc75456f3d2
Quin'darius Lyles-Woods <quinwoodz@gmail.com>
2021-07-18 11:47:02 -0400

    Getting closer to being done

--- mod3/hangman.py
+++ mod3/hangman.py
@@ -1,7 +1,20 @@
 word_to_guess = ["F","O","X"]
 word_guessed = ["","",""]
 
-letter_guessed = input("Guess a Letter: ")
 
-print("The letter you guessed is " + letter_guessed)
+def guess_letter(letter):
+    try:
+        return word_to_guess.index(letter)
+    except ValueError:
+        print("Thats not in the list! Try again...")
+        return -1
+
+while True:
+    letter_guessed = input("Guess a Letter: ")
+    print("The letter you guessed is " + letter_guessed)
+    if(guess_letter(letter_guessed) != -1):
+        print("holy shit")
+        word_guessed[guess_letter(letter_guessed)] = letter_guessed
+    print("Your Guessed Letters are ", word_guessed)
+
 
